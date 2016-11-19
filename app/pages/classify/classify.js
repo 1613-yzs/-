364a04263efd07fbb04f196cce0bd88d0a645b6a
feifei <724277441@qq.com>
2016-11-19 15:55:44 +0800

    wyf classify-all  up

--- app/pages/classify/classify.js
+++ app/pages/classify/classify.js
@@ -6,6 +6,23 @@ angular.module('projectDemo.classifyPage',[])
     name:'classify',
     url:'/classify',
     css:'app/pages/classify/classify.css',
-    templateUrl:'app/pages/classify/classify.html'
+    templateUrl:'app/pages/classify/classify.html',
+    //controller:'classifyCtrl'
   })
+  .state({
+    name:'classify.clAll',
+    url:'/clAll',
+
+    css:'app/pages/classify/classify.clAll/classify.clAll.css',
+    
+    templateUrl:'app/pages/classify/classify.clAll/classify.clAll.html',
+    //需要添加index上面的css样式 、防止点击这个影响上一级css样式
+    controller:function($css){
+      $css.add('app/common/css/reset.css');
+      $css.add('app/common/css/main.css');
+      $css.add('app/pages/classify/classify.css');
+    }
+  })
+  
+  
 })
