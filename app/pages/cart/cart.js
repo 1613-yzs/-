14770477482abd30a1a44f4c519dcee2f1e4d4c0
feifei <724277441@qq.com>
2016-11-21 11:56:28 +0800

    index

--- app/pages/cart/cart.js
+++ app/pages/cart/cart.js
@@ -6,6 +6,6 @@ angular.module('projectDemo.cartPage',[])
     name:'cart',
     url:'/cart',
     css:'app/pages/cart/cart.css',
-    templateUrl:'app/pages/cart/cart.html'
+    templateUrl:'app/pages/cart/cart.html',
   })
 })
