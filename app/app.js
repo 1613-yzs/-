caa4b6bfb55393f3cb6058bd69a4af5a176bb3a3
狂拽 <448412605@qq.com>
2016-11-22 20:19:55 +0800

    add all

--- app/app.js
+++
@@ -1,9 +0,0 @@
-angular.module('projectDemo',['ui.router','angularCSS',
-'projectDemo.homePage',
-'projectDemo.classifyPage',
-'projectDemo.musicPage',
-'projectDemo.cartPage',
-'projectDemo.minePage'])
-.config(function($stateProvider,$urlRouterProvider){
-  $urlRouterProvider.otherwise('/home')
-})
