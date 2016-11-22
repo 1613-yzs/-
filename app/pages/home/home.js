302388ba37eaffeff3a91f0d940821294536a4a0
狂拽 <448412605@qq.com>
2016-11-22 22:07:03 +0800

    chenage and add

--- app/pages/home/home.js
+++ app/pages/home/home.js
@@ -1,6 +1,7 @@
 angular.module('projectDemo.homePage',[])
 .config(function($stateProvider,$urlRouterProvider){
   $stateProvider
+  //首页
   .state({
     name:'home',
     url:'/home',
@@ -8,27 +9,28 @@ angular.module('projectDemo.homePage',[])
     templateUrl:'app/pages/home/home.html',
     controller:'homeCtrl',
   })
+  //镇店之宝
 .state({
     name:'home_good',
     url:'/home_good',
-    css:'app/pages/home_good/good.css',
-    templateUrl:'app/pages/home_good/good.html',
+    css:'app/pages/home/home_good/good.css',
+    templateUrl:'app/pages/home/home_good/good.html',
     controller:function($css,$scope,$location){
     	carousel();
     	goTop()
-      $css.add('app/pages/home_good/good.css')
+      $css.add('app/pages/home/home_good/good.css')
 					
 				$scope.click = function(){
 				$location.url("/home")
 				}
     }
 })
-
+//空气感
 .state({
     name:'home_air',
     url:'/home_air',
-    css:'app/pages/home_air/air.css',
-    templateUrl:'app/pages/home_air/air.html',
+    css:'app/pages/home/home_air/air.css',
+    templateUrl:'app/pages/home/home_air/air.html',
     controller:function($css,$scope,$location){
     	carousel();
     	goTop()
@@ -39,20 +41,56 @@ angular.module('projectDemo.homePage',[])
 				}
     }
 })
-
+//百万试用
 .state({
     name:'home_try',
     url:'/home_try',
-    css:'app/pages/home_try/try.css',
-    templateUrl:'app/pages/home_try/try.html',
+    css:'app/pages/home/home_try/try.css',
+    templateUrl:'app/pages/home/home_try/try.html',
     controller:function($css,$scope,$location){
-//  	carousel();
-//  	goTop()
       $css.add('app/pages/home_try/try.css')
-					
-//				$scope.click = function(){
-//				$location.url("/home")
-//				}
+    }
+})
+//每日签到
+.state({
+    name:'home_sign',
+    url:'/home_sign',
+    css:'app/pages/home/home_sign/sign.css',
+    templateUrl:'app/pages/home/home_sign/sign.html',
+    controller:function($css,$scope,$location){
+      $css.add('app/pages/home_sign/sign.css')
+    }
+})
+//新品推荐
+.state({
+    name:'home_new',
+    url:'/home_new',
+    css:'app/pages/home/home_new/new.css',
+    templateUrl:'app/pages/home/home_new/new.html',
+    controller:function($css,$scope,$location){
+      $css.add('app/pages/home_new/new.css');
+      carousel();
+      goTop()
+    }
+})
+//品牌故事
+.state({
+    name:'home_story',
+    url:'/home_story',
+    css:'app/pages/home/home_story/story.css',
+    templateUrl:'app/pages/home/home_story/story.html',
+    controller:function($css,$scope,$location){
+      $css.add('app/pages/home_story/story.css');
+    }
+})
+//三颗柚
+.state({
+    name:'home_threeGrapefruit',
+    url:'/home_threeGrapefruit',
+    css:'app/pages/home/home_threeGrapefruit/threeGrapefruit.css',
+    templateUrl:'app/pages/home/home_threeGrapefruit/threeGrapefruit.html',
+    controller:function($css,$scope,$location){
+      $css.add('app/pages/home_threeGrapefruit/threeGrapefruit.css');
     }
 })
 })
