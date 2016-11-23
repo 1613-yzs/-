94d11ff6803b9cac6384fa168cdec4d7c7cd8494
feifei <724277441@qq.com>
2016-11-23 10:44:50 +0800

    ssss

--- app/pages/classify/classify.clAll/classify.clAll.js
+++ app/pages/classify/classify.clAll/classify.clAll.js
@@ -1,11 +1,1 @@
-//angular.module('projectDemo.cartPage',[])
-//.config(function($stateProvider,$urlRouterProvider){
-//
-//$stateProvider
-//.state({
-//  name:'classify.clAll',
-//  url:'/clAll',
-//  css:'app/pages/classify/classify.clAll/classify.clAll.css',
-//  templateUrl:'app/pages/classify/classify.clAll/classify.clAll.html'
-//})
-//})
+
